54752f7126b48dd935b6b6fcd192567da522d5be
Sahar <saharhalim151@gmail.com>
2024-01-12 11:39:49 -0500

    twoSum in javascript

--- string/twoSum.js
+++ string/twoSum.js
@@ -1,36 +1,44 @@
 function twoSum(nums, target) {
-  // let j = 0;
-  // let i= 1;
-  // const sortedArr = nums.sort((a,b) => {a - b})
 
-  // while(i !== j) {
-  //   const sum = sortedArr[i] + sortedArr[j];
-  //   console.log(sum)
-  //   if (sum === target) {
-  //     return [j,i];
-  //   }
-  //   else if (sum < target) {
-  //     i++;
-  //   }
-  //   else {
-  //     j++;
+  // Brute force
+  // for(let i=0; i < nums.length; i++) {
+  //   for(let j=i+1; j <nums.length; j++){
+  //     if(nums[i]+nums[j] === target && i!=j){
+  //       return [i,j];
+  //     }
   //   }
   // }
 
-  for(let i=0; i < nums.length; i++) {
-    for(let j=i+1; j <nums.length; j++){
-      if(nums[i]+nums[j] === target && i!=j){
-        return [i,j];
-      }
+  //Two pointers
+  const numsWithIndex = nums.map((num, index) => [num, index]);
+  console.log(numsWithIndex);
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const sum = nums[left] + nums[right];
+    if (sum === target) {
+      return [left, right];
+    }
+    if (sum > target) {
+      right--;
+    }
+    else {
+      left++;
     }
   }
+  return []
 }
+// const numsWithIndex = nums.map((num, index) => [num, index]);
+// console.log(numsWithIndex);
+
+
+
 
 // Testing
 
 
 
-console.log(twoSum([2,7,11,15],9))
-console.log(twoSum([2,5,5,11],10))
-console.log(twoSum([3,2,4],6))
-console.log(twoSum([3,3],6))
+console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([2, 5, 5, 11], 10))
+console.log(twoSum([3, 2, 4], 6))
+console.log(twoSum([3, 3], 6))

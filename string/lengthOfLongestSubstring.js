511ace5c671e4dbb1dc26301f021d22065ebf701
Sahar <saharhalim151@gmail.com>
2024-01-17 11:20:05 -0500

    lengthOfLongestSubstring with  comments

--- string/lengthOfLongestSubstring.js
+++ string/lengthOfLongestSubstring.js
@@ -1,17 +1,29 @@
 function lengthOfLongestSubstring(s) {
-  let currentString= []
+  // Initialise an array to store the running characters and a longest string length variable
+  let currentString = [];
   let longestStringLength = 0;
 
-  for(let i =0; i<s.length; i++) {
-   const currentCharacterPosition = currentString.indexOf(s[i])
-    if(currentCharacterPosition !== -1) {
-      currentString.splice(0, currentCharacterPosition +1)
-    } 
+  // Loop through the provided string
+  for (let i = 0; i < s.length; i++) {
+    // Attempt to get the current character's position in the current string
+    const currentCharacterPosition = currentString.indexOf(s[i]);
+
+    // Check if the current character exists in the current string
+    if (currentCharacterPosition !== -1) {
+      // Chop the array off after the occurence of the character
+      currentString.splice(0, currentCharacterPosition + 1);
+    }
+
+    // Add the current character to the array
     currentString.push(s[i]);
 
-    longestStringLength = Math.max(longestStringLength , currentString.length)
-    
+    // Store the current string length if bigger than the existing record
+    longestStringLength = Math.max(
+      longestStringLength,
+      currentString.length
+    );
   }
+
   return longestStringLength;
 }
 

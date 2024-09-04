b1d41f2c35e67ec06fe18a21d11eecc2ef980cf3
Sahar <saharhalim151@gmail.com>
2024-09-04 19:55:44 -0400

    fibonacci

--- string/fibonacci.js
+++ string/fibonacci.js
@@ -4,7 +4,12 @@ function fibonacci(prev1, prev2) {
 
   for(let i=0; i<=18; i++ ) {
      newfib = prev1+prev2;
-     
+     prev2= prev1;
+     prev1=newfib;
+     console.log(prev2)
     
   }
+  return prev2;
 }
+
+console.log(fibonacci(0,1))

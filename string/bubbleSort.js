8e1466b8eab46544777d5bc49f9ad9021898f1bb
Sahar <saharhalim151@gmail.com>
2024-09-11 09:46:41 -0400

    bubble sort

--- string/bubbleSort.js
+++ string/bubbleSort.js
@@ -1,14 +1,14 @@
-function bubble(n) {
-  array=[];
+function bubble(array) {
+  n=array.length;
   
-  for(let j=0; j<=n-1; j++) {
-    console.log("hghdwehd")
-    for(let i =j; i <= n-j-1; i++) {
-      console.log("fffff")
-      if(n[i] > n[i+1]){
+  for(let j=0; j<=array.length-1; j++) {
+    
+    for(let i =j; i <= array.length-j-1; i++) {
+      
+      if(array[i] > array[i+1]){
         
-        [n[i], n[i+1]] = [n[i+1], n[i]];
-      return  console.log(n[i], n[i+1] = n[i+1], n[i])
+        [array[i], array[i+1]] = [array[i+1], array[i]];
+      
       }
       else {
         i++;
@@ -16,7 +16,7 @@ function bubble(n) {
       }
     }
   }
-  return n
+  return array
 
 }
 
